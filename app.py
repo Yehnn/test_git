d0c269611a2bc8e41129fbac2271f96401c725fc
Yehnn <34642019+Yehnn@users.noreply.github.com>
2018-07-24 19:25:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 def hello():
         return 'Hello Shiyanlou002!'
 
-    if __name__ == "__main__":
-            app.run(host="0.0.0.0", port=8001, debug=True)
+if __name__ == "__main__":
+        app.run(host="0.0.0.0", port=8001, debug=True)
